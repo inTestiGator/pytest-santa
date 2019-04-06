63740c3e3a228c8e5451bf26bb0789e64c02f917
enpuyou <youe2@allegheny.edu>
2019-04-06 01:33:55 -0400

    Fix a lint error

--- example/termfrequency/runtermfreq.py
+++ example/termfrequency/runtermfreq.py
@@ -1,7 +1,7 @@
 """Run script for termfrequency"""
 # !/usr/bin/env python
 import sys
-from cp_termfrequency import WordFrequencyController
+from termfrequency.cp_termfrequency import WordFrequencyController
 
 
 WordFrequencyController(sys.argv[1]).run()
