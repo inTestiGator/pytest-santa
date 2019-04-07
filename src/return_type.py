c8bd41629942c2d8849228241fafde00503aa2f6
enpuyou <youe2@allegheny.edu>
2019-04-07 16:01:59 -0400

    change the type return to dict

--- src/return_type.py
+++ src/return_type.py
@@ -19,12 +19,11 @@ def return_type(function):
         if not row:
             conn.commit()
         dict_row = json.loads(row[0])  # convert the string into dictionary
-        print(dict_row.keys())  # return the parameter
+        # print(dict_row.keys())  # return the parameter
+        types = {}
         for k, v in dict_row.items():
-            types = ""
-            types += v['qualname']
-            print(v['qualname'])
-            return types
+            types[k] = v['qualname']
+        return types  # return the parameter and its type in a dict type
     except sqlite3.Error as e:
         print("Database error: %s" % e)
     except Exception as e:  # pylint: disable=W0703
@@ -36,5 +35,8 @@ def return_type(function):
 
 if __name__ == '__main__':
     # return_type("monkeytype_call_traces", "termfrequency")
-    return_type(function="\"StopWordManager.is_stop_word\"")
-    return_type(function="\"WordFrequencyManager.sorted\"")
+    types_isstopwords = return_type(function="\"StopWordManager.is_stop_word\"")
+    print(types_isstopwords)
+    print("=================")
+    types_sorted = return_type(function="\"WordFrequencyManager.sorted\"")
+    print(types_sorted)
