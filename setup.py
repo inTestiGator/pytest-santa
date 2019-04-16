212aa1256903cb4c81d04145caea881f14d78d6c
Sweta <rauniyars@allegheny.edu>
2019-04-16 16:12:06 -0400

    Updated the setup file

--- setup.py
+++ setup.py
@@ -2,10 +2,22 @@
 from setuptools import setup
 
 setup(
-    name="pytest-santa",
-    packages=["pytest-santa"],
-    # the following makes a plugin available to pytest
-    entry_points={"pytest11": ["pytest-santa = pytest-santa.pluginmodule"]},
-    # custom PyPI classifier for pytest plugins
-    classifiers=["Framework :: Pytest"],
+    name='pytest-santa',
+    version='0.1.0',
+    description='Infers types and generates test data from the respective type with
+                Monkeytype and Hypothesis.''',
+    long_description=open("README.md").read(),
+    license="GNU License",
+    author='Enpu You, Haeley Griffin, Sweta Rauniyar, Alex Korzeniwsky, Issac Barrezueta',
+    author_email='youe2@allegheny.edu, griffinh@allegheny.edu, barrezuetai@allegheny.edu, korzeniwskya@allegheny.edu',
+    url='https://github.com/inTestiGator/pytest-santa',
+    py_modules=[
+        'pytest-santa',
+    ],
+    entry_points={
+        'pytest11': ['pytest-santa = pytest-santa'],
+    },
+    install_requires=[
+        'pytest>=3.5',
+    ],
 )
