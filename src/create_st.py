e6e75bebdadabecad78953f8f2901951f2cef04e
enpuyou <youe2@allegheny.edu>
2019-04-16 17:19:08 -0400

    Add the types that needs to be handled

--- src/create_st.py
+++ src/create_st.py
@@ -3,9 +3,45 @@
 import hypothesis.strategies as st
 
 
-def create_st(type_data):
-    """ Create the compatible kwargs for strategies"""
+def read_type(type_dict):
+    """ Read type from the return types and convert into a list"""
     if type is None:  # TODO: will be a exception handling here.
         print("There is no type passed in")
     else:
-        pass
+        type_list = list(type_dict.values())
+    return type_list
+
+
+def create_st(type_list):
+    """ Create the compatible kwargs for strategies"""
+    for t in type_list:
+        if t == 'int':
+            pass
+        elif t == 'str':
+            pass
+        elif t == 'float':
+            pass
+        elif t == 'bool':
+            pass
+        elif t == 'byte':
+            pass
+        elif t == 'object':
+            pass
+        elif t == 'List':
+            pass
+        elif t == 'Dict':
+            pass
+        elif t == 'Tuple':
+            pass
+        elif t == 'Union':
+            pass
+        elif t == 'Set':
+            pass
+        elif t == 'type':
+            pass
+
+
+if __name__ == '__main__':
+    input_type = {"var" : "int", "boo" : "int"}
+    output = read_type(input_type)
+    assert output == ['int', 'int']
--- tests/test_return_type.py
+++ tests/test_return_type.py
@@ -1,7 +1,11 @@
-from src.create_st import create_st
+from src import create_st
+
+
+def test_read_type():
+    input_type = {"var" : "int", "boo" : "int"}
+    output = create_st.read_type(input_type)
+    assert output == ['int', 'int']
 
 
 def test_create_st():
-    input_type = {"var" : "int"}
-    output = create_st(input_type)
-    assert output == 'st.integers()'
+    input_type = ['int', 'int']
