988271397eda6258357fe1fb786d1652f21f2ada
enpuyou <youe2@allegheny.edu>
2019-04-18 16:24:46 -0400

    ran black

--- example/tests/test_termfrequency.py
+++ example/tests/test_termfrequency.py
@@ -63,7 +63,9 @@ def test_WordFrequencyManager_info():
 
 def test_WordFrequencyController_run():
     """ Checks that the run() works by asserting the output equals to the expected"""
-    frequency_controller = tf_objectoriented.WordFrequencyController("example/inputs/input.txt")
+    frequency_controller = tf_objectoriented.WordFrequencyController(
+        "example/inputs/input.txt"
+    )
     word_freqs = frequency_controller.run()
     assert word_freqs == [
         ("live", 2),
