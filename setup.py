ec261aed096540368587f7be159d7cba9fda84d1
enpuyou <youe2@allegheny.edu>
2019-04-19 23:48:22 -0400

    Fix bugs in setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
-# sample ./setup.py file
 from setuptools import setup
 
 setup(
-    name='pytest-santa',
-    version='0.1.0',
-    description='Infers types and generates test data from the respective type with
-                Monkeytype and Hypothesis.''',
+    name="pytest-santa",
+    version="0.1.0",
+    description="Infers types and generates test data from the respective type with Monkeytype and Hypothesis.",
     long_description=open("README.md").read(),
-    license="GNU License",
-    author='Enpu You, Haeley Griffin, Sweta Rauniyar, Alex Korzeniwsky, Issac Barrezueta',
-    author_email='youe2@allegheny.edu, griffinh@allegheny.edu, barrezuetai@allegheny.edu, korzeniwskya@allegheny.edu',
-    url='https://github.com/inTestiGator/pytest-santa',
+    author="Enpu You, Haeley Griffin, Sweta Rauniyar, Alex Korzeniwsky, Issac Barrezueta",
+    author_email="youe2@allegheny.edu, griffinh@allegheny.edu, rauniyars@allegheny.edu, barrezuetai@allegheny.edu, korzeniwskya@allegheny.edu",
+    url="https://github.com/inTestiGator/pytest-santa",
+    license="GNU",
+    platforms="any",
     py_modules=[
-        'pytest-santa',
+        "pytest-santa",
     ],
     entry_points={
-        'pytest11': ['pytest-santa = pytest-santa'],
+        'pytest11': ['santa = pytest-santa'],
     },
     install_requires=[
         'pytest>=3.5',
